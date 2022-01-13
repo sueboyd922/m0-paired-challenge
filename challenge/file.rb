240e3ff2f678018a1b4ebc3ddb2ad6df39dd06a4
Susan Boyd <sueboyd922@gmail.com>
2022-01-12 22:25:06 -0500

    Add file.rb and update index.md

---
+++ challenge/file.rb
@@ -0,0 +1,13 @@
+# chose name because we did not read ahead. it said make a file so we did. now we will call it "our_library"
+# assign favorite_book to Catch 22
+favorite_book = "Catch 22"
+# assign variable for how many books we have
+number_of_books = 7
+shelves_full = true
+# variable for how fast we read
+pages_per_minute = 1.33
+least_favorite = "The Odyssey"
+hate_reading = false
+
+# assign variable harry_potter to array containing abbreviations of all the books
+harry_potter = ["S.S", "CoS", "PoA", "GoF", "OotP", "HBP", "DH"]
---
+++ challenge/index.md
@@ -0,0 +1,8 @@
+# Books
+1. Catch 22
+2. Harry Potter
+3. Pillars of the Earth
+4. Sirens of Titan
+5. Game of Thrones
+
+Susan was driving, Andrew navigated. It went pretty smoothly, we would have fixed our file naming because we did not read ahead. Could have made a better name. 
